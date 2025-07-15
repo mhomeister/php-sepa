69b7b9fdbdd8d1e9e3f10800aa5f5215473d3dd5
M. Homeister <micha@0x25.de>
2025-07-15 22:54:48 +0200

    updated package folder structure

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'oneawebmarketing/php-sepa',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '49fe267f6d769c540a66f114e969ad7a2d186fec',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'oneawebmarketing/php-sepa' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '49fe267f6d769c540a66f114e969ad7a2d186fec',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
